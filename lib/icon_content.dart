12c2cdbd80feaf54637d29407d1977c86da29887
KevSalcedo <kevsalcedom@gmail.com>
2023-06-01 23:24:22 -0500

    Add constants.dart file and update constant names.

--- lib/icon_content.dart
+++ lib/icon_content.dart
@@ -1,9 +1,5 @@
 import 'package:flutter/material.dart';
-
-const labelTextStyle = TextStyle(
-  fontSize: 18.0,
-  color: Color(0xFF8D8E98),
-);
+import 'constants.dart';
 
 class IconWidget extends StatelessWidget {
   IconWidget({required this.icon, required this.iconName});
@@ -25,7 +21,7 @@ class IconWidget extends StatelessWidget {
         ),
         Text(
           iconName,
-          style: labelTextStyle,
+          style: kLabelTextStyle,
         ),
       ],
     );
--- lib/input_page.dart
+++ lib/input_page.dart
@@ -2,11 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'icon_content.dart';
 import 'reusable_card.dart';
-
-const bottomContainerHeight = 80.0;
-const activeCardColour = Color(0xFF1D1E33);
-const inactiveCardColour = Color(0xFF111328);
-const buttonSubmitColor = Color(0xFFEB1555);
+import 'constants.dart';
 
 enum Gender {
   male,
@@ -21,16 +17,16 @@ class InputPage extends StatefulWidget {
 class _InputPageState extends State<InputPage> {
   Gender? selectedGender;
 
-  Color maleCardColour = inactiveCardColour;
-  Color femaleCardColour = inactiveCardColour;
+  Color maleCardColour = kInactiveCardColour;
+  Color femaleCardColour = kInactiveCardColour;
 
   void updateColor(Gender selectedGender) {
     selectedGender == Gender.male
-        ? maleCardColour = activeCardColour
-        : maleCardColour = inactiveCardColour;
+        ? maleCardColour = kActiveCardColour
+        : maleCardColour = kInactiveCardColour;
     selectedGender == Gender.female
-        ? femaleCardColour = activeCardColour
-        : femaleCardColour = inactiveCardColour;
+        ? femaleCardColour = kActiveCardColour
+        : femaleCardColour = kInactiveCardColour;
   }
 
   @override
