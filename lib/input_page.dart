49dd88ac273dd91097aa42617ce0e3c78e834332
KevSalcedo <kevsalcedom@gmail.com>
2023-06-01 23:26:39 -0500

    Create RoundIconButton.

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -234,3 +234,30 @@ class _InputPageState extends State<InputPage> {
     );
   }
 }
+
+class RoundIconButton extends StatelessWidget {
+  RoundIconButton({required this.icon, required this.onPressed});
+
+  final IconData icon;
+  final Function onPressed;
+
+  @override
+  Widget build(BuildContext context) {
+    return RawMaterialButton(
+      onPressed: () {
+        onPressed();
+      },
+      elevation: 6.0,
+      constraints: const BoxConstraints.tightFor(
+        width: 56.0,
+        height: 56.0,
+      ),
+      shape: const CircleBorder(),
+      fillColor: const Color(0xFF4C4F5E),
+      child: Icon(
+        icon,
+        color: Colors.white,
+      ),
+    );
+  }
+}
